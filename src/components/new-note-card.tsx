97d3220b25629cd25e092d2297ac9978bdee42ab
Tiago Santos <tiago881@gmail.com>
2024-04-24 18:18:55 -0300

    feat: Create Dialog.Portal in new-note-card component

--- src/components/new-note-card.tsx
+++ src/components/new-note-card.tsx
@@ -1,9 +1,10 @@
 import * as Dialog from "@radix-ui/react-dialog";
+import { X } from "lucide-react";
 
 export function NewNoteCard() {
   return (
     <Dialog.Root>
-      <Dialog.Trigger className="rounded-md bg-slate-700 p-5 space-y-3 overflow-hidden relative">
+      <Dialog.Trigger className="rounded-md flex flex-col bg-slate-700 text-left p-5 gap-3">
         <span className="text-sm font-medium text-slate-200">
           Adicionar nota
         </span>
@@ -12,6 +13,32 @@ export function NewNoteCard() {
           Grave uma nota em áudio que será convertido em texto automaticamente
         </p>
       </Dialog.Trigger>
+
+      <Dialog.Portal>
+        <Dialog.Overlay className="inset-0 fixed bg-black/50" />
+        <Dialog.Content
+          className="fixed overflow-hidden left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[640px]
+           w-full h-[60vh] bg-slate-700 rounded-md flex flex-col outline-none"
+        >
+          <Dialog.Close className="absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100">
+            <X className="size-5" />
+          </Dialog.Close>
+          <div className="flex flex-1 flex-col gap-3 p-5">
+            <span className="text-sm font-medium text-slate-300"></span>
+            <p className="text-sm leading-6 text-slate-400"></p>
+          </div>
+
+          <button
+            type="button"
+            className="w-full bg-slate-800 py-4 text-center text-sm text-slate-300 outline-none font-medium group"
+          >
+            Deseja{" "}
+            <span className="text-red-400 group-hover:underline">
+              apagar essa nota?
+            </span>
+          </button>
+        </Dialog.Content>
+      </Dialog.Portal>
     </Dialog.Root>
   );
 }
