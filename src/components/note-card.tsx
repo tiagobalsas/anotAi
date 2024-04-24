522b615dd1d405b6a6b1e18263abd27a0116906d
Tiago Santos <tiago881@gmail.com>
2024-04-24 15:55:07 -0300

    feat: Add Note interface and update props in app.tsx

--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -1,19 +1,20 @@
-export function NoteCard() {
+interface NoteCardProps {
+  note: {
+    date: Date;
+    content: string;
+  };
+}
+
+export function NoteCard({ note }: NoteCardProps) {
   return (
     <button
-      className="rounded-md text-left bg-slate-800 p-5 space-y-3 overflow-hidden relative 
+      className="rounded-md text-left flex flex-col bg-slate-800 p-5 gap-3 overflow-hidden relative 
       hover:ring-2 hover:ring-slate-600 outline-none focus-visible:ring-2 focus-visible:ring-lime-400"
     >
-      <span className="text-sm font-medium text-slate-300">há 2 dias</span>
-      <p className="text-sm leading-6 text-slate-400">
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi quis
-        ducimus ut labore velit enim aspernatur minima debitis voluptatibus
-        repudiandae libero, nobis, animi, voluptatum rerum assumenda eum
-        repellendus nisi numquam. Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Ratione quo molestiae in, dolorum dolore repellendus
-        optio doloremque at, deleniti fugit magni dolor similique harum cumque.
-        Dolorem quas id doloremque pariatur?
-      </p>
+      <span className="text-sm font-medium text-slate-300">
+        {note.date.toISOString()}
+      </span>
+      <p className="text-sm leading-6 text-slate-400">{note.content}</p>
 
       <div
         className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 
