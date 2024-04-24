5f4330145c73da78998cb39f7b18b6a0e834fd9c
Tiago Santos <tiago881@gmail.com>
2024-04-24 18:03:42 -0300

    feat: Install lucide-react library and add Dialog.Close component

--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -1,6 +1,7 @@
 import * as Dialog from "@radix-ui/react-dialog";
 import { formatDistanceToNow } from "date-fns";
 import { ptBR } from "date-fns/locale";
+import { X } from "lucide-react";
 
 interface NoteCardProps {
   note: {
@@ -32,6 +33,9 @@ export function NoteCard({ note }: NoteCardProps) {
           className="fixed overflow-hidden left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[640px]
            w-full h-[60vh] bg-slate-700 rounded-md flex flex-col outline-none"
         >
+          <Dialog.Close className="absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100">
+            <X className="size-5" />
+          </Dialog.Close>
           <div className="flex flex-1 flex-col gap-3 p-5">
             <span className="text-sm font-medium text-slate-300">
               {formatDistanceToNow(note.date, {
