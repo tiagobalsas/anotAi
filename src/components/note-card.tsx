18c413ea92210cfd183b9963e295413df364ebd2
Tiago Santos <tiago881@gmail.com>
2024-04-24 17:14:16 -0300

    feat: Add delete note button to modal

--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -29,10 +29,10 @@ export function NoteCard({ note }: NoteCardProps) {
       <Dialog.Portal>
         <Dialog.Overlay className="inset-0 fixed bg-black/50" />
         <Dialog.Content
-          className="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[640px] w-full h-[60vh]
-         bg-slate-700 rounded-md flex flex-col outline-none"
+          className="fixed overflow-hidden left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[640px]
+           w-full h-[60vh] bg-slate-700 rounded-md flex flex-col outline-none"
         >
-          <div className=" flex felx-1 flex-col gap-3 p-5">
+          <div className="flex flex-1 flex-col gap-3 p-5">
             <span className="text-sm font-medium text-slate-300">
               {formatDistanceToNow(note.date, {
                 locale: ptBR,
@@ -41,6 +41,16 @@ export function NoteCard({ note }: NoteCardProps) {
             </span>
             <p className="text-sm leading-6 text-slate-400">{note.content}</p>
           </div>
+
+          <button
+            type="button"
+            className="w-full bg-slate-800 py-4 text-center text-sm text-slate-300 outline-none font-medium group"
+          >
+            Deseja{" "}
+            <span className="text-red-400 group-hover:underline">
+              apagar essa nota?
+            </span>
+          </button>
         </Dialog.Content>
       </Dialog.Portal>
     </Dialog.Root>
