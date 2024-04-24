47de693f13c51ddd668b975751034a5d96a7f715
Tiago Santos <tiago881@gmail.com>
2024-04-24 16:26:05 -0300

    feat: Create and style modal with Radix library

--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -1,3 +1,5 @@
+import * as Dialog from "@radix-ui/react-dialog";
+
 interface NoteCardProps {
   note: {
     date: Date;
@@ -7,19 +9,30 @@ interface NoteCardProps {
 
 export function NoteCard({ note }: NoteCardProps) {
   return (
-    <button
-      className="rounded-md text-left flex flex-col bg-slate-800 p-5 gap-3 overflow-hidden relative 
+    <Dialog.Root>
+      <Dialog.Trigger
+        className="rounded-md text-left flex flex-col bg-slate-800 p-5 gap-3 overflow-hidden relative 
       hover:ring-2 hover:ring-slate-600 outline-none focus-visible:ring-2 focus-visible:ring-lime-400"
-    >
-      <span className="text-sm font-medium text-slate-300">
-        {note.date.toISOString()}
-      </span>
-      <p className="text-sm leading-6 text-slate-400">{note.content}</p>
+      >
+        <span className="text-sm font-medium text-slate-300">
+          {note.date.toISOString()}
+        </span>
+        <p className="text-sm leading-6 text-slate-400">{note.content}</p>
 
-      <div
-        className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 
+        <div
+          className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 
       pointer-events-none"
-      />
-    </button>
+        />
+      </Dialog.Trigger>
+      <Dialog.Portal>
+        <Dialog.Overlay className="inset-0 fixed bg-black/50" />
+        <Dialog.Content
+          className="fixed left-1/2 top-1/2 -translate-x-1/2 trasnlat-y-1/2 max-w-[640px] w-full
+         bg-slate-700 rounded-md flex flex-col outline-none"
+        >
+          outline
+        </Dialog.Content>
+      </Dialog.Portal>
+    </Dialog.Root>
   );
 }
