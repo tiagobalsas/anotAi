c1cbdc83ebd87fbd693beb4dfc7c20a22c72344e
Tiago Santos <tiago881@gmail.com>
2024-04-27 15:54:25 -0300

    style: Add responsive padding to main container in app.tsx

--- src/app.tsx
+++ src/app.tsx
@@ -49,7 +49,7 @@ export function App() {
       : notes;
 
   return (
-    <div className="mx-auto max-w-6xl my-12 space-y-6">
+    <div className="mx-auto max-w-6xl my-12 space-y-6 px-5 md:px-0">
       <img src={logo} alt="nlw-expert" />
 
       <form className="w-full">
